9c44f83aee58a9f326251ab658f3881104f1654e
sdidd <77382851+sdidd@users.noreply.github.com>
2023-08-13 19:25:07 +0530

    Add files via upload

---
+++ src/main/java/com/register/java/SkamRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.register.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkamRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkamRegisterApplication.class, args);
+	}
+
+}
