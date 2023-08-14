edb9a0b174fce650be5975269f706c9e66fc851f
sdidd <sdiddtayade@gmail.com>
2023-08-14 12:24:46 +0530

    removed some stuff have to redo

--- src/main/java/com/register/java/model/userInterface.java
+++ src/main/java/com/register/java/model/userInterface.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.register.java.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,13 +6,3 @@ public interface userInterface extends JpaRepository<user, Integer>
 {
 	user findByUsername(String username);
 }
-=======
-package com.register.java.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface userInterface extends JpaRepository<user, Integer> 
-{
-	user findByUsername(String username);
-}
->>>>>>> 9c44f83aee58a9f326251ab658f3881104f1654e
--- src/main/java/com/register/java/registrationController.java
+++ src/main/java/com/register/java/registrationController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.register.java;
 
 
@@ -12,16 +11,16 @@ import com.register.java.model.userInterface;
 
 @RestController
 public class registrationController {
-	
+
 	@Autowired
 	private userInterface userinterface;
-	
+
 	@RequestMapping("/check")
 	public String check()
 	{
 		return "Registered";
 	}
-	
+
 	@RequestMapping("/register-user/{username}/{password}/{email}")
 	public String registerUser(@PathVariable("username") String username,@PathVariable("password") String password,@PathVariable("email") String email)
 	{
@@ -30,48 +29,9 @@ public class registrationController {
 		u.setUsername(username);
 		u.setPassword(password);
 		u.setEmail(email);
-		
+
 		userinterface.save(u);
 		return "Registered";
 	}
-	
+
 }
-=======
-package com.register.java;
-
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.register.java.model.user;
-import com.register.java.model.userInterface;
-
-@RestController
-public class registrationController {
-	
-	@Autowired
-	private userInterface userinterface;
-	
-	@RequestMapping("/check")
-	public String check()
-	{
-		return "Registered";
-	}
-	
-	@RequestMapping("/register-user/{username}/{password}/{email}")
-	public String registerUser(@PathVariable("username") String username,@PathVariable("password") String password,@PathVariable("email") String email)
-	{
-		user u =  new user();
-		u.setId(4);
-		u.setUsername(username);
-		u.setPassword(password);
-		u.setEmail(email);
-		
-		userinterface.save(u);
-		return "Registered";
-	}
-	
-}
->>>>>>> 9c44f83aee58a9f326251ab658f3881104f1654e
